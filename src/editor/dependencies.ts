421b8e73747dba5a2509754f3c981b142800f0b0
Ana Silva <ana.i.silva@accenture.com>
2024-01-29 14:51:46 +0100

    recommended extension installation implemended

---
+++ src/editor/dependencies.ts
@@ -0,0 +1,49 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { extensions } from "vscode";
+import { editorWorkspace } from "./workspace";
+import { editorInput } from "./input";
+import { editorCommands } from "./commands";
+
+enum RecommendedExtensionsInputOptions {
+    INSTALL = "Install",
+    NOT_NOW = "Not Now",
+    DONT_ASK_AGAIN = "Do not show again"
+};
+
+async function activateExtensionDependencies(dependencies: string | string[]){
+    
+    const missingExtDependencies: string[] = 
+        [dependencies]
+            .flat()
+            .filter((dependencyName: string) => !extensions.getExtension(dependencyName));
+
+    if(missingExtDependencies.length){
+
+        const suggestRecommendedExtensions: string | boolean = 
+            editorWorkspace.handleWorkspaceConfiguration("recommendedExtensions", true).get();
+
+        if(suggestRecommendedExtensions){
+            const message: string = "There are some recommended extensions that can enhance your usage of SFMC DevTools. Would you like to install them?";
+            const options: string[] = Object.values(RecommendedExtensionsInputOptions);
+            const selectedOption: string | undefined = await editorInput.handleShowOptionsMessage(message, options);
+            if(selectedOption){
+                if(selectedOption === RecommendedExtensionsInputOptions.INSTALL){
+                    missingExtDependencies.forEach((extDependency: string) => {
+                        editorCommands.executeCommand(
+                            ["extension.open", "workbench.extensions.installExtension"], 
+                            [extDependency]
+                        );
+                    });
+                }else if(selectedOption === RecommendedExtensionsInputOptions.DONT_ASK_AGAIN){
+                    editorWorkspace.handleWorkspaceConfiguration("recommendedExtensions", false).set();
+                }
+            }
+        }
+    }
+}
+
+const editorDependencies = {
+    activateExtensionDependencies
+};
+
+export { editorDependencies };
