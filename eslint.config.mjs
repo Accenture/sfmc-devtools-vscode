5080f976f345f8d98f53a100b52d02710bd0b028
Jörn Berkefeld <joern.berkefeld@accenture.com>
2026-03-10 15:46:25 +0100

    #0 upgrade eslint flat config to not us compat mode

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,38 +1,15 @@
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import { defineConfig, globalIgnores } from "eslint/config";
-import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import prettier from "eslint-plugin-prettier";
+import tseslint from "typescript-eslint";
 import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-	baseDirectory: __dirname,
-	recommendedConfig: js.configs.recommended,
-	allConfig: js.configs.all
-});
 
 export default defineConfig([
+	js.configs.recommended,
+	eslintPluginPrettierRecommended,
+	tseslint.configs.recommended,
 	globalIgnores(["**/dist", "src/**/*.d.ts", "**/webpack.config.js", "**/node_modules", "**/dist"]),
 	{
-		extends: fixupConfigRules(
-			compat.extends(
-				"plugin:@typescript-eslint/recommended",
-				"prettier",
-				"plugin:prettier/recommended",
-				"plugin:import/typescript"
-			)
-		),
-
-		plugins: {
-			"@typescript-eslint": fixupPluginRules(typescriptEslint),
-			prettier: fixupPluginRules(prettier)
-		},
-
 		languageOptions: {
 			parser: tsParser,
 			ecmaVersion: 6,
