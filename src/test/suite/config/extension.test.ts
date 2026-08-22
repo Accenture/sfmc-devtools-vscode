acbf8e58f26e701ad6929f77c0eb40c7921c2ff2
Jörn Berkefeld <joern.berkefeld@gmail.com>
2026-08-22 10:51:12 +0200

    #0 3.4.0

--- src/test/suite/config/extension.test.ts
+++ src/test/suite/config/extension.test.ts
@@ -83,9 +83,9 @@ suite("Config – extension", () => {
 		);
 	});
 
-	test("package.json version is 3.3.0", () => {
+	test("package.json version is 3.4.0", () => {
 		const manifest = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), "package.json"), "utf8"));
-		assert.strictEqual(manifest.version, "3.3.0");
+		assert.strictEqual(manifest.version, "3.4.0");
 	});
 
 	test("delayTimeUpdateStatusBar is a positive number", () => {
